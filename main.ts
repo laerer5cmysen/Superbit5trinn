bb3da6a469180acda1e7ec93961e967cb19fbef4
laerer5cmysen <99287788+laerer5cmysen@users.noreply.github.com>
2022-02-10 12:21:49 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
 basic.forever(function () {
+    servos.P1.run(25)
+})
+basic.forever(function () {
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P6, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P6, 0)
+})
+basic.forever(function () {
     if (sonar.ping(
     DigitalPin.P15,
     DigitalPin.P14,
     PingUnit.Centimeters
-    ) < 5) {
+    ) < 10) {
         basic.pause(500)
         servos.P0.setAngle(90)
     } else {
